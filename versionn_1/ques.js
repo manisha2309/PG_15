b8da7dac42d8df71ab7955b921b4c118e96b7d57
Manisha <156739896+manisha2309@users.noreply.github.com>
2024-05-15 02:45:38 +0530

    Update ques.js

--- versionn_1/ques.js
+++ versionn_1/ques.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let question = document.querySelectorAll(".question");
 
 question.forEach(question => {
@@ -17,23 +16,3 @@ question.forEach(question => {
     }
   })
 })
-=======
-let question = document.querySelectorAll(".question");
-
-question.forEach(question => {
-  question.addEventListener("click", event => {
-    const active = document.querySelector(".question.active");
-    if(active && active !== question ) {
-      active.classList.toggle("active");
-      active.nextElementSibling.style.maxHeight = 0;
-    }
-    question.classList.toggle("active");
-    const answer = question.nextElementSibling;
-    if(question.classList.contains("active")){
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    } else {
-      answer.style.maxHeight = 0;
-    }
-  })
-})
->>>>>>> 9c7a04d8b0eb27886e8c0394d1ceac7e970deaca
